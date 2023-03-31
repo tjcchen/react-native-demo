800850fb4c88245cb124a6b31badc346c57a98aa
Andy Chen <chenyangpoppin@hotmail.com>
2023-03-31 11:33:15 +0800

    feat: adding different assets to this proj

--- app/index.js
+++ app/index.js
@@ -4,20 +4,9 @@ import { Link, Stack } from 'expo-router';
 const Home = () => {
   return (
     <View>
-      <Text>Home1</Text>
+      <Text>Home Let's write some code!</Text>
     </View>
   );
 };
 
 export default Home;
-
-// export default function() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       {/* Use the `Screen` component to configure the layout. */}
-//       <Stack.Screen options={{ title: "Overview" }} />
-//       {/* Use the `Link` component to enable optimized client-side routing. */}
-//       <Link href="/details">Go to Details</Link>
-//     </View>
-//   );
-// }
