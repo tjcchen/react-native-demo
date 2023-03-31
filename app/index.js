b708170b1e9f86ae674d881ae3bd97c551534fc1
Andy Chen <chenyangpoppin@hotmail.com>
2023-03-31 12:28:15 +0300

    feat: build project interface with SafeAreaView

--- app/index.js
+++ app/index.js
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native';
-import { Link, Stack } from 'expo-router';
+import { useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { Link, Stack, useRouter } from 'expo-router';
+
+import { images, icons, COLORS, FONT, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home Let's write some code!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Text style={{ color: COLORS.tertiary }}>Home</Text>
+      <Stack.Screen
+        options={{ 
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+         }}
+      />
+    </SafeAreaView>
   );
 };
 
