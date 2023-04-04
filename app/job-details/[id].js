c9fea788cfb3e7f9b2d3a9a34798ad9a5b5bbdda
Andy Chen <chenyangpoppin@hotmail.com>
2023-04-04 22:24:05 +0800

    feat: adding the job-detail page

---
+++ app/job-details/[id].js
@@ -0,0 +1,16 @@
+/**
+ * A dynamic job_id router handler
+ * 
+ * Note: shortcut for template - rafce
+ */
+import { View, Text } from 'react-native';
+
+const JobDetails = () => {
+  return (
+    <View>
+      <Text>JobDetails</Text>
+    </View>
+  )
+}
+
+export default JobDetails;
