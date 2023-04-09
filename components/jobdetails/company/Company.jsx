ed6aec1468264f38faef36f4630a39d7031e147a
Andy Chen <chenyangpoppin@hotmail.com>
2023-04-09 20:52:41 +0800

    chore: format thing

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -20,19 +20,19 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
       </View>
 
       <View style={styles.jobTitleBox}>
-          <Text style={styles.jobTitle}>{jobTitle}</Text>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
       </View>
       
       <View style={styles.companyInfoBox}>
-          <Text style={styles.companyName}>{companyName} / </Text>
-          <View style={styles.locationBox}>
-            <Image
-              source={icons.location}
-              resizeMode="contain"
-              style={styles.locationImage}
-            />
-            <Text style={styles.locationName}>{location}</Text>
-          </View>
+        <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode="contain"
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
       </View>
     </View>
   )
