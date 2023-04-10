340032e7dfd33a712cd8f2c89500d934b7de5e0d
Andy Chen <chenyangpoppin@hotmail.com>
2023-04-10 10:19:38 +0800

    feat: implement the apply-for-job button logic

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -22,7 +22,7 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
         <Text style={styles.jobName} numberOfLines={1}>
           { job?.job_title }
         </Text>
-        <Text style={styles.jobType}> { job?.job_employment_type }</Text>
+        <Text style={styles.jobType}>{ job?.job_employment_type }</Text>
       </View>
     </TouchableOpacity>
   )
